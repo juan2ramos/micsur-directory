884f507de4712844600f696aefecd342cf0034db
Juan2Ramos <juan2ramos>
2016-07-25 14:23:57 -0500

    image

---
+++ public/js/images.js
@@ -0,0 +1,52 @@
+var $files = $('.images'),
+    $output;
+
+$files.on('change', function () {
+
+    $output = $('.result');
+
+    if (this.files[0].size < 2400000) {
+
+        //$('.preload').removeClass("hidden");
+        uploadImage(this.files[0]);
+
+    } else {
+        alert("El tamaño de la imagen debe ser inferior a 2MB");
+    }
+
+});
+
+$files.on('dragover', function () {
+    $(this).parents('figure').css({
+        'border-style':'dashed'
+    });
+
+});
+$files.on('dragleave', removeElement);
+$files.on('drop', removeElement);
+function removeElement() {
+    $(this).parents('figure').css({
+        'border-style':'solid',
+    });
+}
+function uploadImage(file) {
+
+    if (!file.type.match('image'))
+        return;
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function (event) {
+
+        var picFile = event.target;
+        var figure = document.createElement("span");
+        $output.html('');
+        figure.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+            "title='" + picFile.name + "'/>";
+
+
+        $output.append(figure);
+
+    });
+    reader.readAsDataURL(file);
+
+}
--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -1,35 +1,39 @@
 @extends('layout.front')
 
 @section('content')
-    <form action="{{ route('register') }}" enctype="multipart/form-data"  method="post" class="row Form-register">
+    <form action="{{ route('register') }}" enctype="multipart/form-data" method="post" class="row Form-register">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <h1 class="col-8 small-12  bottom-element">
             DIRECTORIO
             <b>INDUSTRIAS CREATIVAS Y CULTURALES</b>
         </h1>
         <div class="col-4 small-12 row Form-contentImageUser between">
-            <div class="col-3 small-12">Imagen:</div>
+            <div class="col-3 small-12 ">Imagen:</div>
             <figure class="col-9 small-12 Form-figure-user ">
                 {!!  $errors->first('image-profile', '<p class="error">:message</p>')  !!}
-                <input type="file" name="image-profile"  id="image-profile">
+                <output class="result"></output>
+                <input type="file" name="image-profile" class="images" id="image-profile">
+                <figcaption>¡Haz clic o arrastra la imagen!</figcaption>
             </figure>
+
         </div>
         <div class="col-8 offset-4 offset-small-0 small-12">
             <hr class="Form-hr">
             <label for="name" class="row middle">
                 {!!  $errors->first('name', '<p class="error">:message</p>')  !!}
                 <span class="col-5">Nombre:</span>
-                <input class="col-7" name="name" id="name"  value="{{ old('name') }}" type="text">
+                <input class="col-7" name="name" id="name" value="{{ old('name') }}" type="text">
             </label>
             <label for="last-name" class="row middle">
                 {!!  $errors->first('last-name', '<p class="error">:message</p>')  !!}
                 <span class="col-5">Apellido:</span>
-                <input class="col-7" name="last-name"  value="{{ old('last-name') }}" id="last-name" type="text">
+                <input class="col-7" name="last-name" value="{{ old('last-name') }}" id="last-name" type="text">
             </label>
             <label for="identification-number" class="row middle">
                 {!!  $errors->first('identification-number', '<p class="error">:message</p>')  !!}
                 <span class="col-5">Número de identificación:</span>
-                <input class="col-7" name="identification-number" value="{{ old('identification-number') }}" id="identification-number" type="text">
+                <input class="col-7" name="identification-number" value="{{ old('identification-number') }}"
+                       id="identification-number" type="text">
             </label>
             <label for="email" class="row middle">
                 {!!  $errors->first('email', '<p class="error">:message</p>')  !!}
@@ -48,16 +52,24 @@
             <label for="company" class="row middle">
                 {!!  $errors->first('company', '<p class="error">:message</p>')  !!}
                 <span class="col-5">Empresa:</span>
-                <input class="col-7" name="company" value="{{ old('company') }}"  id="company" type="text">
+                <input class="col-7" name="company" value="{{ old('company') }}" id="company" type="text">
             </label>
             <label for="activities" class="row middle">
                 {!!  $errors->first('activities', '<p class="error">:message</p>')  !!}
                 <span class="col-5">Actividad:</span>
                 <select class="col-7" name="activities" id="activities">
                     <option value="">Selecciona una opción</option>
-                    <option value="dd">Selecciona una opción</option>
-                    <option value="d">Selecciona una opción</option>
-                    <option value="d">Selecciona una opción</option>
+                    <option value="audiovisual" {{(old('activities')=='audiovisual')?'selected':''}}>Audiovisual
+                    </option>
+                    <option value="diseno" {{(old('activities')=='diseno')?'selected':''}}>Diseño</option>
+                    <option value="musica" {{(old('activities')=='musica')?'selected':''}}>Música</option>
+                    <option value="editorial" {{(old('activities')=='editorial')?'selected':''}}>Editorial</option>
+                    <option value="animacion_videojuegos" {{(old('activities')=='animacion_videojuegos')?'selected':''}}>
+                        Animación y Videojuegos
+                    </option>
+                    <option value="artes_escenicas" {{(old('activities')=='artes_escenicas')?'selected':''}}>Artes
+                        escénicas
+                    </option>
                 </select>
             </label>
             <label for="what-i-do" class="row middle">
@@ -72,17 +84,22 @@
             <label for="mobile" class="row middle">
                 {!!  $errors->first('mobile', '<p class="error">:message</p>')  !!}
                 <span class="col-5 small-12">Teléfono móvil:</span>
-                <input class="col-1 small-2" value="{{ old('mobile') }}" maxlength="3" name="mobile" id="mobile" type="text">
+                <input class="col-1 small-2" value="{{ old('mobile') }}" maxlength="3" name="mobile" id="mobile"
+                       type="text">
                 <div class="col-6 small-10" style="padding-left: 5px">
-                    <input value="{{ old('mobile-1') }}" class="{{($errors->first('mobile'))?'error':''}}" maxlength="15" name="mobile-1" id="mobile-1" type="text">
+                    <input value="{{ old('mobile-1') }}" class="{{($errors->first('mobile'))?'error':''}}"
+                           maxlength="15" name="mobile-1" id="mobile-1" type="text">
                 </div>
             </label>
             <label for="phone" class="row middle">
                 <span class="col-5 small-12">Teléfono fijo:</span>
-                <input  maxlength="3"  class="col-1 small-2" name="phone" id="phone" value="{{ old('phone') }}"  type="text">
-                <div class="col-1 small-2" style="padding-left: 5px"><input  maxlength="3"  value="{{ old('phone-1') }}" name="phone-1" id="phone-1" type="text">
+                <input maxlength="3" class="col-1 small-2" name="phone" id="phone" value="{{ old('phone') }}"
+                       type="text">
+                <div class="col-1 small-2" style="padding-left: 5px"><input maxlength="3" value="{{ old('phone-1') }}"
+                                                                            name="phone-1" id="phone-1" type="text">
                 </div>
-                <div class="col-5 small-8" style="padding-left: 5px"><input  maxlength="15"  value="{{ old('phone-2') }}"  name="phone-2" id="phone-2" type="text">
+                <div class="col-5 small-8" style="padding-left: 5px"><input maxlength="15" value="{{ old('phone-2') }}"
+                                                                            name="phone-2" id="phone-2" type="text">
                 </div>
             </label>
         </div>
@@ -90,4 +107,7 @@
             <button>REGISTRAR...</button>
         </div>
     </form>
+@endsection
+@section('scripts')
+    <script src="{{asset('js/images.js')}}"></script>
 @endsection
