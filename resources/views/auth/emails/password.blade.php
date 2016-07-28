08e737391b62c78e7ccd68a54058aa65259bf021
Juan2Ramos <juan2ramos>
2016-07-27 21:12:35 -0500

    reset mail

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Haga clic aquí para restablecer la contraseña: <a href="{{ $link = url('public/password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Haga clic aquí para restablecer la contraseña: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
