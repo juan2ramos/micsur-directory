6067752989a653d3b3f04cac3ef8a3af25023699
Juan2Ramos <juan2ramos>
2016-07-27 22:28:00 -0500

    reset mail

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y combinar la confirmación.',
+    'reset' => 'Tu contraseña ha sido restablecida!',
+    'sent' => 'Hemos enviado por email el enlace de restablecimiento de contraseña!',
+    'token' => 'token  invalido.',
     'user' => "No podemos encontrar un usuario con esa dirección ",
 
 ];
